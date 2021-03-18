68f3a325190c72ae0c85912494758d3d2e02c273
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-03-19 01:16:18 +0200

    Finished lecture #46 & #47, added ability to poll all events from event-bus service, and polling to query service, when it comes up. Also did some refactoring, and added error handling to event-bus.

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,15 +5,29 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
+function postEvent(port, event) {
+  axios.post(`http://localhost:${port}/events`, event).catch((err) => {
+    console.log(err.message);
+  });
+}
+
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
-  axios.post('http://localhost:4003/events', event);
+  events.push(event);
+
+  postEvent(4000, event);
+  postEvent(4001, event);
+  postEvent(4002, event);
+  postEvent(4003, event);
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
