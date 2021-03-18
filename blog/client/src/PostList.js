d2363d98b7dd3a6f89774a9cdf83e9efbe8d85e1
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-03-19 01:58:12 +0200

    Finished lecture #48, worked some more on syncing.

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import CommentCreate from "./CommentCreate";
-import CommentList from "./CommentList";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
   const [posts, setPosts] = useState({});
 
   const getPosts = async () => {
-    const res = await axios.get("http://localhost:4002/posts");
+    let res = await axios.get('http://localhost:4002/posts').catch((err) => {
+      console.log(err);
+      return { data: {} };
+    });
 
     setPosts(res.data);
   };
@@ -19,11 +22,11 @@ export default () => {
   const renderedPosts = Object.values(posts).map((post) => {
     return (
       <div
-        className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
+        className='card'
+        style={{ width: '30%', marginBottom: '20px' }}
         key={post.id}
       >
-        <div className="card-body">
+        <div className='card-body'>
           <h3>{post.title}</h3>
 
           <CommentList comments={post.comments} />
@@ -34,7 +37,7 @@ export default () => {
   });
 
   return (
-    <div className="d-flex flex-row flex-wrap justify-content-between">
+    <div className='d-flex flex-row flex-wrap justify-content-between'>
       {renderedPosts}
     </div>
   );
