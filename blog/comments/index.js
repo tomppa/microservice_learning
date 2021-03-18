867f11b5f36b351460c526ee6eba7a29c45b7b9f
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-03-19 00:11:04 +0200

    Finished lecture #41, adding status property to comments.

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
-const cors = require("cors");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,39 +10,40 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", async (req, res) => {
-  const commentId = randomBytes(4).toString("hex");
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const postId = req.params.id;
   const comments = commentsByPostId[postId] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[postId] = comments;
 
-  await axios.post("http://localhost:4005/events", {
-    type: "CommentCreated",
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
     data: {
       id: commentId,
       content,
       postId,
+      status: 'pending',
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Received Event", req.body.type);
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
 
   res.send({});
 });
 
 app.listen(4001, () => {
-  console.log("Listening on 4001");
+  console.log('Listening on 4001');
 });
