f66a0258c0384c471610b313fa8ba43297ee02a4
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-03-19 00:25:57 +0200

    Finished lecture #42, adding logic to comment service for handling moderated comments.

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -38,8 +38,30 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Received Event', req.body.type);
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  console.log('Received Event', type);
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return (comment.id = id);
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        content,
+        status,
+      },
+    });
+  }
 
   res.send({});
 });
