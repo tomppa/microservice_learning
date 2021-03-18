1b04e7909d346a7b1157b52f5a8976fd863420d8
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2021-03-19 00:52:17 +0200

    Finished lecture #44, adding display logic to React app.

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React from 'react';
 
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === 'approved') {
+      content = comment.content;
+    } else if (comment.status === 'pending') {
+      content = 'This comment is awaiting moderation.';
+    } else if (comment.status === 'rejected') {
+      content = 'This comment has been rejected.';
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
